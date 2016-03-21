85a628913c606f5f1d2918d36964b1241df1b08e
Arsen Gasparyan <to.arsen.gasparyan@gmail.com>
2016-03-21 23:53:45 +0300

    Toggle and show thumb filters

--- Filterer/ViewController.swift
+++ Filterer/ViewController.swift
@@ -17,8 +17,20 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             compareButton.enabled = false
             originalLabel.hidden = false
             editButton.enabled = false
+            
+            let filters = [Instagram(filter: "CISepiaTone", deltaKey: kCIInputIntensityKey),
+                Instagram(filter: "CIGammaAdjust", deltaKey: "inputPower"),
+                Instagram(filter: "CIVignetteEffect", deltaKey: "inputIntensity"),
+                Instagram(filter: "CIHueAdjust", deltaKey: "inputAngle"),
+                Instagram(filter: "CIColorMonochrome", deltaKey: "inputIntensity")]
+            
+            filterButton1.setImage(filters[0].apply(originalImage!, delta:  0.5), forState: .Normal)
+            filterButton2.setImage(filters[1].apply(originalImage!, delta:  0.5), forState: .Normal)
+            filterButton3.setImage(filters[2].apply(originalImage!, delta:  0.5), forState: .Normal)
+            filterButton4.setImage(filters[3].apply(originalImage!, delta:  0.5), forState: .Normal)
+            filterButton5.setImage(filters[4].apply(originalImage!, delta:  0.5), forState: .Normal)
         }
-    }
+    } 
     
     private var filteredImage: UIImage? {
         didSet {
@@ -41,7 +53,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var originalLabel: UILabel!
     @IBOutlet weak var editButton: UIButton!
     @IBOutlet weak var editMenu: UIView!
+    
     @IBOutlet weak var filterSlider: UISlider!
+    @IBOutlet weak var filterButton1: UIButton!
+    @IBOutlet weak var filterButton2: UIButton!
+    @IBOutlet weak var filterButton3: UIButton!
+    @IBOutlet weak var filterButton4: UIButton!
+    @IBOutlet weak var filterButton5: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -71,8 +89,21 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imageView.image = filteredImage
         originalLabel.hidden = true
     }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        super.touchesBegan(touches, withEvent: event)
+        showOriginalImage()
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        super.touchesEnded(touches, withEvent: event)
+        if let _ = filteredImage {
+            showFilteredImage()
+        }
+    }
 
     // MARK: Edit
+    
     @IBAction func onEdit(sender: UIButton) {
         if (sender.selected) {
             hideEditMenu()
@@ -88,12 +119,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     // MARK: Share
+    
     @IBAction func onShare(sender: AnyObject) {
         let activityController = UIActivityViewController(activityItems: ["Check out our really cool app", imageView.image!], applicationActivities: nil)
         presentViewController(activityController, animated: true, completion: nil)
     }
     
     // MARK: New Photo
+    
     @IBAction func onNewPhoto(sender: AnyObject) {
         let actionSheet = UIAlertController(title: "New Photo", message: nil, preferredStyle: .ActionSheet)
         
@@ -126,6 +159,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     // MARK: UIImagePickerControllerDelegate
+    
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         dismissViewControllerAnimated(true, completion: nil)
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
@@ -174,6 +208,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     
     // MARK: Filter Menu
+    
     @IBAction func onFilter(sender: UIButton) {
         if (sender.selected) {
             hideSecondaryMenu()
